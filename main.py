9f6690b495a1b08ec8d9e0fbd2c2fde44855f0c7
刘伯栋 <liubodong2010@126.com>
2019-03-02 09:51:22 +0800

    添加图片过小判断；添加url和mid_url得到的图片大小比较，取最大的

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 import urllib.parse as parser
+import math
 
 import requests
 from requests.adapters import HTTPAdapter
@@ -47,7 +48,12 @@ def get_apps_by_tags_from_category(catagory, tags, start=0, count=100):
         'tags': parser.quote(tags)
     }
     try:
-        return session.get(base_url, params=params).json()['data']
+        response = session.get(base_url, params=params, timeout=10).json()
+        total = int(response['total'])
+        return {
+            'total': total,
+            'data': response['data']
+        }
     except Exception as e:
         print(repr(e))
         return None
@@ -62,40 +68,66 @@ def get_apps_by_category(catagory, start=0, count=100):
         'count': count
     }
     try:
-        return session.get(base_url, params=params).json()['data']
+        response = session.get(base_url, params=params, timeout=10).json()
+        total = int(response['total'])
+        return {
+            'total': total,
+            'data': response['data']
+        }
     except Exception as e:
         print(repr(e))
         return None
 
 
-def get_url_content(url):
+def get_url_content(url, timeout):
     try:
-        return session.get(url).content
+        return session.get(url, timeout=timeout).content
     except Exception as e:
         print(repr(e))
         return None
 
 
 if __name__ == "__main__":
+    category = 6 # 美女
     start = 0
-    page_size = 50
+    page_size = 10
     p = os.path.join(save_path)
     if not os.path.exists(p):
-                os.makedirs(p)
-    pics = get_apps_by_category(6, start, page_size)
-    while pics is not None:
-        for pic in pics:
+        os.makedirs(p)
+    response = get_apps_by_category(category, start, page_size)
+    total = response['total']
+    while response is not None and start <= total:
+        for pic in response['data']:
             id = pic['id']
             path = os.path.join(p, str(id) + '.jpg')
             if os.path.exists(path):
                 print('[' + path + '已存在')
                 continue
-            url = pic['url_mid']
-            content = get_url_content(url)
-            if content:
+            url_mid = pic['url_mid']
+            content_mid = get_url_content(url_mid, timeout=10)
+            url = pic['url']
+            content = get_url_content(url, timeout=10)
+            real_content = None
+            if content_mid and content:
+                if len(content_mid) > len(content):
+                    real_content = content_mid
+                else:
+                    real_content = content
+            elif content_mid and not content:
+                real_content = content_mid
+            else:
+                real_content = content
+            if real_content and len(real_content) <= 50 * 1024:
+                print('图片[' + id + ']过小')
+                continue
+            if real_content:
                 with open(path, 'wb+') as f:
-                    f.write(content)
+                    f.write(real_content)
                     f.close()
                 print('已下载[' + id + ']至' + path)
+            else:
+                print('获取[' + id + ']失败')
         start += page_size
-        pics = get_apps_by_category(6, start, page_size)
+        response = get_apps_by_category(category, start, page_size)
+        print('start:' + str(start) + ', total:' + str(total))
+    print('下载完成！')
