5daae63ae8095917b4a17f1359da37cc105a17fd
刘伯栋 <liubodong2010@126.com>
2019-04-10 15:44:52 +0800

    1、增加日志打印； 2、增加忽略title的选项

--- enterdesktop.py
+++ enterdesktop.py
@@ -6,44 +6,51 @@ import requests
 from requests.adapters import HTTPAdapter
 import logging
 
-logging.basicConfig(level=logging.ERROR,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 class EnterDesktop:
-    def __init__(self, save_path=None):
+    def __init__(self, save_path=None, ignore_title=False):
         self.logger = logging.getLogger("EnterDesktop")
         self.session = requests.Session()
-        self.session.mount('http://', HTTPAdapter(max_retries=3))
-        self.session.mount('https://', HTTPAdapter(max_retries=3))
+        self.session.mount('http://', HTTPAdapter(max_retries=2))
+        self.session.mount('https://', HTTPAdapter(max_retries=2))
         self.base_url = 'https://mm.enterdesk.com'
         self.save_path = 'D:\\回车壁纸'
+        self.ignore_title = ignore_title
         self.pool = ThreadPool(4)
 
-    def __get_url_content__(self, url):
+    def __get_url_content__(self, url, headers=None):
         try:
-            content = self.session.get(url, timeout=3).content
+            content = self.session.get(url, headers=headers, timeout=3).content
             return content
         except Exception as e:
-            self.logger.warning(repr(e))
+            self.logger.warning('在获取:' + url + '时出错')
+            self.logger.error(e)
             return None
 
-    def __get_url_text__(self, url, encoding):
-        content = self.__get_url_content__(url)
+    def __get_url_text__(self, url, encoding, headers=None):
+        content = self.__get_url_content__(url, headers)
         if not content:
             return None
         else:
-            return str(content, encoding=encoding)
+            try:
+                return str(content, encoding=encoding)
+            except Exception as e:
+                self.logger.error(e)
+                return None
 
-    def __download__(self, path, src):
+    def __download__(self, path, src, referer):
         if os.path.exists(path):
+            self.logger.info('[' + path + ']已存在')
             return
         else:
-            content = self.__get_url_content__(src)
+            content = self.__get_url_content__(src, {'Referer': referer})
             if content:
                 with open(path, 'wb') as f:
                     f.write(content)
-                    print('下载:[' + src + ']到[' + path + ']')
+                    self.logger.info('下载:[' + src + ']到[' + path + ']')
 
     def download_pictures(self, pic_type):
         base_pic_url = self.base_url + '/' + pic_type + '/'
@@ -52,26 +59,33 @@ class EnterDesktop:
             pic_url = base_pic_url + str(num) + '.html'
             soup = self.__parse_html__(pic_url)
             if soup:
+                self.logger.info('开始下载:' + pic_type + ',第[' + str(num) + ']页')
                 collections = self.__get_collections__(soup)
                 if collections:
                     for collection in collections:
                         title = collection['title']
+                        self.logger.info('开始下载:' + title)
                         href = collection['href']
                         soup = self.__parse_html__(href)
                         for pic in self.__get_pics_from_collection__(soup):
-                            path = os.path.join(self.save_path, title)
+                            path = os.path.join(self.save_path, title) if not self.ignore_title else self.save_path
                             if not os.path.exists(path):
                                 os.makedirs(path)
-                            self.__download__(os.path.join(
-                                path, pic['name']), pic['src'])
+                            if not self.ignore_title:
+                                pic_path = os.path.join(path, pic['name'])
+                            else:
+                                pic_path = os.path.join(path, title + '_' + pic['name'])
+                            self.__download__(pic_path, pic['src'], href)
             else:
+                self.logger.warning('在获取' + pic_url + '时出错，跳出循环')
                 break
             num += 1
 
     def __get_pics_from_collection__(self, soup):
         if soup:
             div = soup.find_all('div', class_='swiper-wrapper')[0]
-            return [{'name': a['src'].split('/')[-1], 'src': a['src'].replace('edpic', 'edpic_source')} for a in div.find_all('a')]
+            return [{'name': a['src'].split('/')[-1], 'src': a['src'].replace('edpic', 'edpic_source')} for a in
+                    div.find_all('a')]
         else:
             return []
 
@@ -92,16 +106,17 @@ class EnterDesktop:
     def start(self):
         arr = [PicType.DALU, PicType.RIHAN, PicType.QINGCHUN, PicType.KEAI]
         for pic_type in arr:
-            print("开始下载:" + pic_type)
+            self.logger.info("开始下载:" + pic_type)
             self.download_pictures(pic_type)
 
 
-class PicType():
+class PicType:
     DALU = 'dalumeinv'
     RIHAN = 'rihanmeinv'
     QINGCHUN = 'qingchunmeinv'
     KEAI = 'keaimeinv'
 
 
-EnterDesktop = EnterDesktop()
-EnterDesktop.start()
+if __name__ == '__main__':
+    EnterDesktop = EnterDesktop(ignore_title=True)
+    EnterDesktop.start()
