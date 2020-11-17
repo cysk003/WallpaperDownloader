1858d3151c3a27c13b62803644ce8959af9ba2d5
Liubodong <liubodong2010@126.com>
2020-11-17 15:08:42 +0800

    优化

--- vmgirls.py
+++ vmgirls.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from os import path
 import os
 import urllib3
-from multiprocessing import Pool, cpu_count
 
 urllib3.disable_warnings()
 
@@ -19,41 +18,25 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 }
 
-ArticleTypes = [
-    {'name': '摄影写真', 'url': '/photography', 'code': 17},  # 摄影写真
-    {'name': '校园美女', 'url': '/campus', 'code': 33},  # 校园美女
-    {'name': '清新美女', 'url': '/fresh', 'code': 25},  # 清新美女
-    {'name': '清纯美女', 'url': '/pure', 'code': 18},  # 清纯美女
-    {'name': '糖水', 'url': '/sweet', 'code': 61},  # 糖水
-    {'name': '随笔', 'url': '/belles-lettres', 'code': 2},  # 随笔
-    {'name': '青春风', 'url': '/youth', 'code': 12}  # 青春风
-]
 
-
-def get_articles(article_info: dict, page: int):
+def get_articles():
     res = []
-    dst_url = base_url+'/wp-admin/admin-ajax.php'
-    params = {
-        'append': 'list-archive',
-        'paged': page,
-        'action': 'ajax_load_posts',
-        'query': article_info['code'],
-        'page': 'cat'
-    }
-    response = session.post(dst_url, data=params,
-                            headers=headers, timeout=(3, 3))
+    dst_url = base_url + '/sitemap.html'
+    response = session.post(dst_url, headers=headers, timeout=(10, 10))
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        articles = soup.find_all('a', class_='media-content', target='_blank')
+        h3 = soup.find('h3', string="最新文章")
+        articles = h3.find_next_sibling('ul')
         if articles:
-            res = [{'title': article['title'], 'href': article['href']}
-                   for article in articles]
+            for article in articles.find_all('a', target='_blank'):
+                res.append(
+                    {'title': article['title'], 'href': base_url + '/' + article['href']})
     return res
 
 
 def get_pics(article: dict):
     res = []
-    response = session.get(article['href'], headers=headers, timeout=(3, 3))
+    response = session.get(article['href'], headers=headers, timeout=(10, 10))
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         div = soup.find('div', class_='nc-light-gallery')
@@ -64,7 +47,7 @@ def get_pics(article: dict):
 
 
 def get_pic_content(pic_url: str):
-    response = session.get(pic_url, headers=headers, timeout=(3, 3))
+    response = session.get(pic_url, headers=headers, timeout=(10, 10))
     if response.status_code == 200:
         return response.content
     else:
@@ -90,20 +73,8 @@ def download(article: dict):
 
 
 if __name__ == "__main__":
-    inf = ""
-    index = 1
-    for article_type in ArticleTypes:
-        print("{}.{}".format(index, article_type['name']))
-        index += 1
-    inf += "\n请输入编号\n"
-    i = int(input(inf)) - 1
-    article_type = ArticleTypes[i]
-    page = 1
-    while(True):
-        articles = get_articles(article_type, page)
-        if articles:
-            for article in articles:
-                download(article)
-                print(article)
-        else:
-            break
+    articles = get_articles()
+    if articles:
+        for article in articles:
+            print(article)
+            download(article)
